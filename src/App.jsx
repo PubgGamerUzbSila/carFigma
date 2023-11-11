9c4534b0ef9ac48c1ea0159c50fe88c60bc1d4d6
Asliddin001 <asultonov040@gmail.com>
2023-11-11 18:12:42 +0500

    button and input component done ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Input } from './components/UI/Input/Input'
+
 
 export const App = () => {
   return (
     <>
-    
+      <Input placeholder="hello" type="text" leftIcon={true} rightIcon={true}/>
     </>
   )
 }
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styles from "./Input.module.css";
+import iconLeft from "../../../assets/search.svg";
+import iconright from "../../../assets/filter.svg";
 
-export const Input = () => {
-  return <div>Input</div>;
+export const Input = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+  leftIcon,
+  rightIcon,
+}) => {
+  return (
+    <div className={styles.inputContainer}>
+      {leftIcon && (
+        <span className={styles.icon}>
+          <img src={iconLeft} alt="Left Icon" className={styles.leftIcon} />
+        </span>
+      )}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={leftIcon ? styles.inputWithLeftIcon : styles.input}
+      />
+      {rightIcon && (
+        <span className={styles.icon}>
+          <img src={iconright} alt="Right Icon" className={styles.rightIcon} />
+        </span>
+      )}
+    </div>
+  );
 };
